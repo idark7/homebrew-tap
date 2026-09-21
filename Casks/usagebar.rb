9433607055cdffe9629a8adeafae587062bbff26
idark7 <er.sudiptabanerjee@gmail.com>
2026-09-21 14:15:01 +0100

    Pin sha256

--- Casks/usagebar.rb
+++ Casks/usagebar.rb
@@ -1,6 +1,6 @@
 cask "usagebar" do
   version "3.0.0"
-  sha256 ""
+  sha256 "d8f0e0b64c7b1e8c4f34ceba5098dbf987dc912182c8f1b12800e7e6121af413"
 
   url "https://github.com/idark7/UsageBar/releases/download/v#{version}/UsageBar-#{version}.dmg"
   name "UsageBar"
